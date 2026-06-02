6309920b966d0cc959306505b5bf2a5796945cb4
Phuong20042910 <phuongpdtse182721@fpt.edu.vn>
2026-06-02 11:27:13 +0700

    Delete

--- scientific_journal_trend_tracker_frontend_flutter_app/android/app/src/main/kotlin/com/example/scientific_journal_trend_tracker_frontend_flutter_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.scientific_journal_trend_tracker_frontend_flutter_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- scientific_journal_trend_tracker_frontend_flutter_app/lib/core/error/failure.dart
+++
@@ -1,9 +0,0 @@
-class Failure {
-  final String message;
-  final int? statusCode;
-
-  Failure(this.message, {this.statusCode});
-
-  @override
-  String toString() => 'Failure(message: $message, statusCode: $statusCode)';
-}

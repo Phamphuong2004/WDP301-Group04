3346f7b7cf730293196ae7e36dbb71fa91e83960
Phuong20042910 <phuongpdtse182721@fpt.edu.vn>
2026-06-05 21:36:01 +0700

    Delete Backend

--- Scientific_Journal_Trend_Tracker_Backend/dist/controllers/AuthController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-export declare class AuthController {
-    static register(req: Request, res: Response): Promise<void>;
-    static login(req: Request, res: Response): Promise<void>;
-    static getCurrentUser(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=AuthController.d.ts.map
--- Scientific_Journal_Trend_Tracker_Backend/dist/controllers/BookmarkController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-export declare class BookmarkController {
-    static getUserBookmarks(req: Request, res: Response): Promise<void>;
-    static checkBookmark(req: Request, res: Response): Promise<void>;
-    static addBookmark(req: Request, res: Response): Promise<void>;
-    static removeBookmark(req: Request, res: Response): Promise<void>;
-}
-//# sourceMappingURL=BookmarkController.d.ts.map
